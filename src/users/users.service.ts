6d68c253edf4793316b28d897761387474f4558b
Edwin Mendoza <eddsreal@gmail.com>
2022-06-08 23:01:52 -0500

    User Signup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { CreateUserDto } from './users.dtos';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(args: CreateUserDto) {
+    const user = this.repo.create(args);
+    return this.repo.save(user);
+  }
+}
