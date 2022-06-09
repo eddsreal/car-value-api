10f3af6ff7f6ae6d51ef1db7357c551ae0be093e
Edwin Mendoza <eddsreal@gmail.com>
2022-06-08 21:32:03 -0500

    Report Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { UserEntity } from './users/users.entity';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [UserEntity],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
