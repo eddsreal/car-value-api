b1d58f29d01019af698bbf1f55b9d1fb10206690
Edwin Mendoza <eddsreal@gmail.com>
2022-06-13 18:06:13 -0500

    Dependency Injection Unit Testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './users.dtos';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    //  create a fake property of the users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: ({ email, password }: CreateUserDto) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
+    //  Testing Module
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
